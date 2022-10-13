7a81d3b645d7872e2092837e845e6a586d5bdd53
aTomWeaver <atomweavermail@gmail.com>
2022-10-12 20:21:03 -0400

    complete matrix gen with output and option of custom row and row size

--- matrix.js
+++ matrix.js
@@ -1,34 +1,60 @@
 const pitchClasses = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-function mod12(num) {
-  while (num > 11) num -= 12;
+let mod12 = (num) => {
+  if (num > 11) {
+    while (num > 11) num -= 12;
+  } else if (num < 0) {
+    while (num < 0) num += 12;
+  }
   return num;
-}
+};
 
 function shuffleArray(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
-return arr;
+  return arr;
 }
 
-function getToneRow(size=12) {
+function generateToneRow(size = 12) {
   const toneRow = shuffleArray(pitchClasses);
   for (let i = size; i < 12; i++) toneRow.pop();
   return toneRow;
 }
 
-function getMatrix(toneRow=getToneRow()) {
+function displayMatrix(matrix) {
+  const rowLength = Math.sqrt(matrix.length)
+  console.log(rowLength)
+  let buffer = [];
+  console.log("[\n");
+  for (let i = 0; i < matrix.length + 1; i++) {
+    if (i > 0 && i % rowLength === 0) {
+      console.log(buffer.join("") + "\n");
+      buffer = [];
+    }
+    matrix[i] > 9
+      ? buffer.push("  " + matrix[i])
+      : buffer.push("   " + matrix[i]);
+  }
+  console.log("]");
+}
+
+function generateMatrix(toneRow) {
   let matrix = [...toneRow];
   let buffer = [...toneRow];
-  for (let i = 0; i < toneRow.length; i++) {
-    buffer = buffer.map((elm) => elm = mod12(elm + 1))
+  for (let i = 0; i < toneRow.length - 1; i++) {
+    let inversionInterval = matrix[i] - matrix[i + 1];
+    buffer = buffer.map((elm) => mod12(elm += inversionInterval));
     matrix.push(...buffer);
   }
   return matrix;
 }
 
-console.log(getMatrix())
+function getMatrix(toneRow = generateToneRow(), rowSize) {
+  if (rowSize) toneRow = generateToneRow(rowSize)
+  let newMatrix = generateMatrix(toneRow);
+  displayMatrix(newMatrix);
+}
 
-export {getMatrix}
+export { getMatrix };
--- sbeve.js
+++ sbeve.js
@@ -1,28 +1,43 @@
 #!/usr/bin/env node
 import db from "./sbeveDB.json" assert { type: "json" };
+import { getMatrix } from "./matrix.js";
 
 const getRandElm = (ary) => ary[Math.floor(Math.random() * ary.length)];
-const getPrompt = () => '>> create something ' + getRandElm(db.adjective) + ' and ' + getRandElm(db.music.aspect) + " " + getRandElm(db.music.descriptor);
+const getPrompt = () =>
+  ">> create something " +
+  getRandElm(db.adjective) +
+  " and " +
+  getRandElm(db.music.aspect) +
+  " " +
+  getRandElm(db.music.descriptor);
 const getEnoCard = () => enoSays[0] + getRandElm(db.eno) + enoSays[1];
-const enoSays = ['>> Eno says:\n\t ¸¸♬·¯·♩¸¸♪·¯·♫¸¸', '¸¸♫·¯·♪¸¸♩·¯·♬¸¸']
+const enoSays = [">> Eno says:\n\t ¸¸♬·¯·♩¸¸♪·¯·♫¸¸", "¸¸♫·¯·♪¸¸♩·¯·♬¸¸"];
 
 function parse() {
   let stagedStrings = [];
   const args = process.argv;
-  const flags = args.filter(arg => arg.split("").includes("-"));
+  const flags = args.filter((arg) => arg.split("").includes("-"));
   if (args[2] === undefined) {
     stagedStrings.push(getEnoCard());
     stagedStrings.push(getPrompt());
-  } else if (args.includes('-p')) {
+  } else if (args.includes("-prompt")) {
     stagedStrings.push(getPrompt());
-  } else if (args.includes('-e')) {
+  } else if (args.includes("-eno")) {
     stagedStrings.push(getEnoCard());
+  } else if (args.includes('-matrix')) {
+    if (args[3]) {
+      let row = args[3].split(",")
+      row = row.map(elm => parseInt(elm.trim()))
+      getMatrix(row);
+    } else {
+      getMatrix();
+    }
   }
-  render(stagedStrings)
+  render(stagedStrings);
 }
 
 function render(strings) {
-  console.log("\n" + strings.join("\n\n") + "\n")
+  console.log("\n" + strings.join("\n\n") + "\n");
 }
 
-parse()
+parse();
